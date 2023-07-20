f3b67ccea0d97bf87d96b09eb5af8f9223b06b41
alirezaheibati <alirezaheibati91@gmail.com>
2023-07-20 19:44:09 +0330

    bug fixed on login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,77 @@
+import { useRef, useState } from "react";
 import classes from "./Login.module.css";
 import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 import Input from "../FrequentlyUsed/Input";
 import LoadingSpinner from "../FrequentlyUsed/LoadingSpinner";
+import useHttp from "../../hooks/use-http";
+import Modal from "../FrequentlyUsed/Modal";
+
 const Login = (props) => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const { error, isLoading, sendRequest } = useHttp();
+  const [modalVisibility, setModalVisibility] = useState(true);
+
+  const modalCloseHandler = () => {
+    setModalVisibility(false);
+  };
+
+  const loginUserDataHandler = (result) => {
+    console.log(result);
+  };
+
+  const userLoginHandler = (e) => {
+    e.preventDefault();
+    setModalVisibility(true);
+    if (
+      emailInputRef.current.value.trim().length > 0 &&
+      passwordInputRef.current.value.trim().length > 0
+    ) {
+      sendRequest(
+        {
+          url: `https://parseapi.back4app.com/login?username=${emailInputRef.current.value}&password=${passwordInputRef.current.value}`,
+
+          headers: {
+            "X-Parse-Application-Id":
+              "VUBEmOkNcXSUnYBMnOj68tnzu1jnkopyO6Ow2OGb",
+
+            "X-Parse-REST-API-Key": " NbyD4dNV7pNxrzzCRXQXLUd6cb8C2776i53CBSgW",
+
+            "X-Parse-Revocable-Session": 1,
+
+            "Content-Type": "application/json",
+          },
+        },
+        loginUserDataHandler
+      );
+    }
+  };
+
   const formChangeHandler = () => {
     props.onChangeVisiblFrom("register");
   };
   return (
     <div className={classes["login_form_container"]}>
+      {isLoading && <LoadingSpinner />}
+      {modalVisibility && error && (
+        <Modal title="Error" description={error} onClick={modalCloseHandler} />
+      )}
       <h2>Welcome back!</h2>
       <small>start manageing your tasks faster and better.</small>
-      <form className={classes["login_form_from"]}>
+      <form className={classes["login_form_from"]} onSubmit={userLoginHandler}>
         <Input
           icon={faEnvelope}
           type="mail"
           id="login-form_username"
           placeholder="you@example.com"
+          ref={emailInputRef}
         />
         <Input
           icon={faLock}
           type="password"
           id="login-form_password"
           placeholder="At least 8 characters"
+          ref={passwordInputRef}
         />
 
         <p>Forget Password?</p>
