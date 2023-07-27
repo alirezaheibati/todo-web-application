b3ac8775982ace3190813e53118b90672421516a
alirezaheibati <alirezaheibati91@gmail.com>
2023-07-27 22:10:10 +0330

    task remove functionality been developed

--- src/components/ColumnView/TaskColumnView.jsx
+++ src/components/ColumnView/TaskColumnView.jsx
@@ -8,13 +8,47 @@ import {
 import { faFlag } from "@fortawesome/free-regular-svg-icons";
 import TaskInformation from "../TaskInformation/TaskInformation";
 import { useState } from "react";
+import useHttp from "../../hooks/use-http";
+import { useDispatch, useSelector } from "react-redux";
+import { userInfoSliceActions } from "../../store";
 const TaskColumnView = (props) => {
+  const { isLoading, error, sendRequest } = useHttp();
   const [taskInfoVisibility, setTaskInfoVisibility] = useState(false);
+  const dispatch = useDispatch();
+  const userData = useSelector((store) => store.userInfo);
   const taskInfoShowHandler = () => {
     setTaskInfoVisibility(true);
   };
   const taskInfoCloseHandler = () => {
     setTaskInfoVisibility(false);
+  };
+  const removeTaskResultHandler = (data) => {
+    console.log(data);
+    dispatch(userInfoSliceActions.updateUserInfo());
+  };
+  const removeTaskHandler = (e) => {
+    e.stopPropagation();
+    sendRequest(
+      {
+        url: `https://parseapi.back4app.com/users/${localStorage.getItem(
+          "userId"
+        )}`,
+        method: "PUT",
+        headers: {
+          "X-Parse-Application-Id": "VUBEmOkNcXSUnYBMnOj68tnzu1jnkopyO6Ow2OGb",
+
+          "X-Parse-REST-API-Key": " NbyD4dNV7pNxrzzCRXQXLUd6cb8C2776i53CBSgW",
+          "X-Parse-Session-Token": localStorage.getItem("sessionToken"),
+          "Content-Type": "application/json",
+        },
+        body: {
+          userTasks: userData.info.userTasks.filter(
+            (task) => task.taskId !== props.id
+          ),
+        },
+      },
+      removeTaskResultHandler
+    );
   };
   return (
     <>
@@ -48,7 +82,7 @@ const TaskColumnView = (props) => {
                 <FontAwesomeIcon icon={faPenToSquare} />
                 <p>Edit</p>
               </li>
-              <li>
+              <li onClick={removeTaskHandler}>
                 <FontAwesomeIcon icon={faRemove} style={{ color: "red" }} />
                 <p>Remove</p>
               </li>
